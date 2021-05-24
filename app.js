cfb429807d2514de15946518b8507114f5d27abf
vikasgupta01 <vikasgupta2552@gmail.com>
2021-05-24 20:26:05 +0530

    add compose section

--- app.js
+++ app.js
@@ -30,6 +30,10 @@ app.get("/contact", function (req, res) {
   res.render("contact", { contactPara: contactContent });
 });
 
+app.get("/compose", function (req, res) {
+  res.render("compose");
+});
+
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
