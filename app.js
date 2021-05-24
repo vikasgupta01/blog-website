b5a31b9fba933e15992a2a02d69b47b8c4a2a538
vikasgupta01 <vikasgupta2552@gmail.com>
2021-05-24 21:17:10 +0530

    Add post functionality in compose page

--- app.js
+++ app.js
@@ -34,6 +34,14 @@ app.get("/compose", function (req, res) {
   res.render("compose");
 });
 
+app.post("/compose", function (req, res) {
+  const inputText = req.body.inputText; // this comes from bodyparser module (now deprecated)
+
+  console.log(inputText);
+
+  res.redirect("/compose");
+});
+
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
