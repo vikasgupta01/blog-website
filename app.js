73386d51455054b231529987e6300b0389ff5297
vikasgupta01 <vikasgupta2552@gmail.com>
2021-07-08 21:21:55 +0530

    Add mongoDB-mongoose support for data persistence

--- app.js
+++ app.js
@@ -3,29 +3,50 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require('mongoose');
 
 // Load the full build. (lodash)
 const _ = require('lodash');
 const { xor } = require("lodash");
 
-const homeStartingContent =
-  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent =
-  "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent =
-  "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
 
-let posts = [];
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+
+mongoose.connect('mongodb+srv://vikasgupta01:EatKitkatPls@gettingstarted.g891c.mongodb.net/blogWebsiteDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const postSchema = new mongoose.Schema({
+  title : String,
+  body : String
+});
+
+const Post = new mongoose.model("Post", postSchema);
+
+// let posts = [];
+
+const homeStartingContent =
+  "This is homepage of the website, you can see all the blog posts here. Click on 'Read More' to open full post.";
+const aboutContent =
+  "Hi, this is a simple blogging platform. You can read pre-exiting blogs from homepage and then navigate to the post-specific page by clicking on 'Read More'. You can also create blogs from compose section, and after you submit, it gets reflected in homepage. It was created using node-express for backend, mongoDB-mongoose for database and EJS for views. You can see the code on 'https://github.com/vikasgupta01/blog-website'.";
+const contactContent =
+  "Drop us a email on Email ID : public.vikasgupta@gmail.com or Dial : +91-99xyz-xyz99";
+
+
 app.get("/", function (req, res) {
-  res.render("home", { homePara: homeStartingContent, posts: posts});
+  Post.find({}, function(err, foundPosts) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("home", {homePara : homeStartingContent, posts: foundPosts});
+    }
+  })
+  // res.render("home", { homePara: homeStartingContent, posts: posts});
 });
 
 app.get("/about", function (req, res) {
@@ -41,21 +62,28 @@ app.get("/compose", function (req, res) {
 });
 
 app.post("/compose", function (req, res) {
-  const post = {title : req.body.postTitle, body : req.body.postBody, route : _.kebabCase(req.body.postTitle)};
-  posts.push(post);
+  // const post = {title : req.body.postTitle, body : req.body.postBody, route : _.kebabCase(req.body.postTitle)};
+  const newPost = new Post({
+    title : req.body.postTitle,
+    body : req.body.postBody
+  });
 
-  res.redirect("/");
+  newPost.save(() => res.redirect('/'));
+  // posts.push(blog);
+
+  // res.redirect("/");
 });
 
 // uses dynamic url
-app.get("/posts/:postTitle", function(req, res) {
-  const postTitle = req.params.postTitle;
-  console.log(postTitle);
-  posts.forEach(post => {
-    if (_.lowerCase(post.title)===_.lowerCase(postTitle)) {
-      res.render("post", {title: post.title, content: post.body});
+app.get("/posts/:postID", function(req, res) {
+  const postID = req.params.postID;
+  Post.findOne({_id: postID}, function(err, foundPost) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("post", {title: foundPost.title, content: foundPost.body, _id: postID})
     }
-  });
+  })
 });
 
 
@@ -63,6 +91,6 @@ app.get("/home", function(req, res) {
   res.redirect("/");
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server started on port 3000");
 });
