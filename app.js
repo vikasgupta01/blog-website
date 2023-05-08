e56890919e7df8952c04b370dd870356ffa11c50
Vikas Gupta <vikasgupta2552@gmail.com>
2023-05-09 03:16:57 +0530

    package updates and minor changes in app.js

--- app.js
+++ app.js
@@ -3,27 +3,27 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Load the full build. (lodash)
-const _ = require('lodash');
+const _ = require("lodash");
 const { xor } = require("lodash");
 
-
 const app = express();
-
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-
-mongoose.connect('mongodb+srv://vikasgupta01:EatKitkatPls@gettingstarted.g891c.mongodb.net/blogWebsiteDB', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(
+  "mongodb+srv://vikasgupta01:EatKitkatPls@gettingstarted.g891c.mongodb.net/blogWebsiteDB",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
 const postSchema = new mongoose.Schema({
-  title : String,
-  body : String
+  title: String,
+  body: String,
 });
 
 const Post = new mongoose.model("Post", postSchema);
@@ -35,17 +35,16 @@ const homeStartingContent =
 const aboutContent =
   "Hi, this is a simple blogging platform. You can read pre-exiting blogs from homepage and then navigate to the post-specific page by clicking on 'Read More'. You can also create blogs from compose section, and after you submit, it gets reflected in homepage. It was created using node-express for backend, mongoDB-mongoose for database and EJS for views. You can see the code on 'https://github.com/vikasgupta01/blog-website'.";
 const contactContent =
-  "Drop us a email on Email ID : public.vikasgupta@gmail.com or Dial : +91-99xyz-xyz99";
-
+  "Drop us a email on Email ID : public.vikasgupta2552@gmail.com or Dial : +91-99xyz-xyz99";
 
 app.get("/", function (req, res) {
-  Post.find({}, function(err, foundPosts) {
+  Post.find({}, function (err, foundPosts) {
     if (err) {
       console.log(err);
     } else {
-      res.render("home", {homePara : homeStartingContent, posts: foundPosts});
+      res.render("home", { homePara: homeStartingContent, posts: foundPosts });
     }
-  })
+  });
   // res.render("home", { homePara: homeStartingContent, posts: posts});
 });
 
@@ -64,30 +63,33 @@ app.get("/compose", function (req, res) {
 app.post("/compose", function (req, res) {
   // const post = {title : req.body.postTitle, body : req.body.postBody, route : _.kebabCase(req.body.postTitle)};
   const newPost = new Post({
-    title : req.body.postTitle,
-    body : req.body.postBody
+    title: req.body.postTitle,
+    body: req.body.postBody,
   });
 
-  newPost.save(() => res.redirect('/'));
+  newPost.save(() => res.redirect("/"));
   // posts.push(blog);
 
   // res.redirect("/");
 });
 
 // uses dynamic url
-app.get("/posts/:postID", function(req, res) {
+app.get("/posts/:postID", function (req, res) {
   const postID = req.params.postID;
-  Post.findOne({_id: postID}, function(err, foundPost) {
+  Post.findOne({ _id: postID }, function (err, foundPost) {
     if (err) {
       console.log(err);
     } else {
-      res.render("post", {title: foundPost.title, content: foundPost.body, _id: postID})
+      res.render("post", {
+        title: foundPost.title,
+        content: foundPost.body,
+        _id: postID,
+      });
     }
-  })
+  });
 });
 
-
-app.get("/home", function(req, res) {
+app.get("/home", function (req, res) {
   res.redirect("/");
 });
 
